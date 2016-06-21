39f9f81acb47af50fae60d1e66759cc5377c8245
fangxianzheng <993529786@qq.com>
2016-06-21 20:44:36 +0800

    改进表单验证

---
+++ app.js
@@ -0,0 +1,167 @@
+
+(function(win){
+
+    var Fvalidate = function(formId){
+
+        this.myForm = document.getElementById(formId);
+        this.resultErrorMessage = [];
+        this.testHook = {
+            // 是否为必填
+            required: function(field) {
+                var value = field.value;
+
+                //判断是不是单选框，多选框的可能
+                if(field.type === 'checkbox' || field.type === 'radio'){
+                    return field.checked === true;
+                }
+                return value !== null && value !== '';
+            },
+            maxLength: function(field, length){
+                var value = field.value;
+                return value.length <= length;
+            },
+            minLength: function(field, length){
+                var value = field.value;
+                return value.length >= length;
+            }
+
+        };
+    };
+
+    Fvalidate.prototype = {
+        verification: function(name, obj, callback){
+
+            var inputEle = this.myForm[name] || '';
+
+            //存放取到的验证规则和方法
+            var allRule = [], allMessage = [], errorRule=[], errorMessage=[], oneValid;
+
+            //如果input元素不存在，停止验证
+            if(inputEle === ''){
+                return ;
+            }
+
+            //循环出验证规则和信息
+            for(var i=0; i < obj.rules.length; i++){
+                allRule[i] = obj.rules[i];
+                allMessage[i] = obj.messages[i];
+                var result;
+
+                //当规则不是字符串(是正则)时
+                if(typeof allRule[i] != 'string'){
+                    result =  allRule[i].test(inputEle.value);
+                    if(result === false){
+                        errorRule.push(allRule[i]);
+                        errorMessage.push(allMessage[i]);
+
+                    }
+                }else{     //当规则不是正则时
+
+                    var ruleArr = /(\w+)/ig.exec(allRule[i]);
+
+                    //不带参数的规则处理
+                    if(ruleArr[1] === ruleArr.input){
+                         result = this.testHook[ruleArr.input](inputEle);
+                         if(result === false){
+                             errorRule.push(allRule[i]);
+                             errorMessage.push(allMessage[i]);
+                         }
+
+                    }else{
+                        //带参数的规则处理，如：maxLength
+                        ruleArr = /(\w+)\((\d+)/ig.exec(allRule[i]);
+                        result = this.testHook[ruleArr[1]](inputEle, ruleArr[2]);
+                        if(result === false){
+                            errorRule.push(allRule[i]);
+                            errorMessage.push(allMessage[i]);
+                        }
+                    }
+
+                }
+
+            }
+
+            //只选择一条错误信息推到全局验证
+
+            oneValid = errorMessage.length === 0;
+
+            if(!oneValid){
+                this.resultErrorMessage.push(errorMessage[0])
+            }
+
+            //创建错误信息元素
+            var errorEle = document.createElement('span');
+            errorEle.className = 'errorMessage ' + name + '_errorMessage';
+            if(!oneValid){
+                errorEle.innerHTML = errorMessage[0];
+            }
+
+
+            //默认将错误元素插入input后面
+            if(inputEle.nextSibling){
+                inputEle.parentNode.insertBefore(errorEle,inputEle.nextSibling)
+            }else{
+                inputEle.parentNode.appendChild(errorEle)
+            }
+
+            //运行回调函数，将对应元素和它的错误信息传入，以便可以修改默认样式或增添样式
+            callback(inputEle, errorEle, oneValid)
+
+        },
+        valid: function(){
+            return this.resultErrorMessage.length === 0;
+        }
+
+    };
+
+    return win.Fvalidate = Fvalidate;
+
+})(window, undefined);
+
+
+
+
+
+var o = new Fvalidate('myForm')
+
+
+
+o.verification ('user', {
+    rules: ['required',/^[\u4e00-\u9fa5]+$/i],
+    messages:  ['这项必须填','必须为中文']
+},function(ele, errorEle, oneValid){
+    ele.style.borderBottomColor = 'red'
+    errorEle.style.color = 'red';
+});
+
+
+o.verification ('sure', {
+    rules: ['required'],
+    messages:  ['这项必须填']
+},function(){
+
+});
+
+o.verification ('mobile', {
+    rules: ['required','maxLength(11)','minLength(5)'],
+    messages:  ['这项必须填','手机号超过最大长度','长度太短']
+},function(ele, errorEle, valid){
+    if(!valid){
+        ele.style.borderBottomColor = 'red';
+        errorEle.style.color = 'red';
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
