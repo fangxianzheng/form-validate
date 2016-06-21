1068b0b0626dcf7205899ebe6915a2d529366a4e
fangxianzheng <993529786@qq.com>
2016-06-22 01:04:42 +0800

    新的表单验证

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 (function(win){
 
-    var Fvalidate = function(formId){
+    var Fvalidate = function(formId, arr){
 
         this.myForm = document.getElementById(formId);
         this.resultErrorMessage = [];
+        this.arr = arr;
+        this.allErrorRules = {};
+        this.allErrorMessages = {};
         this.testHook = {
             // 是否为必填
             required: function(field) {
@@ -26,12 +29,71 @@
             }
 
         };
+        this.verification(this.arr);
     };
 
     Fvalidate.prototype = {
-        verification: function(name, obj, callback){
+        verification: function(arr){
 
-            var inputEle = this.myForm[name] || '';
+            //循环出每个input的验证详细信息
+            for(var i = 0, j = arr.length; i < j; i++){
+
+                var inputName = arr[i].name;
+                var inputRules = arr[i].rules;
+                var inputMessages = arr[i].messages;
+                var inputIgnore = arr[i].ignore;
+
+
+
+                //循环出每条验证规则
+                for(var k = 0; k < inputRules.length; k++){
+
+                    var result;
+                    var inputEle = this.myForm[inputName];
+                    //当规则不是字符串(是正则)时
+                    if(typeof inputRules[k] != 'string'){
+                        result = inputRules[k].test(inputEle.value);
+                        if(!result){
+                            this.allErrorRules[inputName] = this.allErrorRules[inputName] || [];
+                            this.allErrorRules[inputName].push(inputRules[k]);
+                            this.allErrorMessages[inputName] = this.allErrorMessages[inputName] || [];
+                            this.allErrorMessages[inputName].push(inputMessages[k]);
+                        }
+                    }else{       //当规则是字符串时
+
+                        var ruleArr = /(\w+)/ig.exec(inputRules[k]);
+                        //不带参数
+                        if(ruleArr[1] === ruleArr.input){
+                            result = this.testHook[ruleArr.input](inputEle);
+                            if(!result){
+                                this.allErrorRules[inputName] = this.allErrorRules[inputName] || [];
+                                this.allErrorRules[inputName].push(inputRules[k]);
+                                this.allErrorMessages[inputName] = this.allErrorMessages[inputName] || [];
+                                this.allErrorMessages[inputName].push(inputMessages[k]);
+                            }
+                        }else{
+                            //带参数的规则处理，如：maxLength
+                            ruleArr = /(\w+)\((\d+)/ig.exec(inputRules[k]);
+                            result = this.testHook[ruleArr[1]](inputEle, ruleArr[2]);
+                            if(!result){
+                                this.allErrorRules[inputName] = this.allErrorRules[inputName] || [];
+                                this.allErrorRules[inputName].push(inputRules[k]);
+                                this.allErrorMessages[inputName] = this.allErrorMessages[inputName] || [];
+                                this.allErrorMessages[inputName].push(inputMessages[k]);
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            console.log(this.allErrorRules)
+            console.log(this.allErrorMessages)
+
+
+
+
+/*            var inputEle = this.myForm[name] || '';
 
             //存放取到的验证规则和方法
             var allRule = [], allMessage = [], errorRule=[], errorMessage=[], oneValid;
@@ -105,11 +167,8 @@
             }
 
             //运行回调函数，将对应元素和它的错误信息传入，以便可以修改默认样式或增添样式
-            callback(inputEle, errorEle, oneValid)
+            callback(inputEle, errorEle, oneValid)*/
 
-        },
-        valid: function(){
-            return this.resultErrorMessage.length === 0;
         }
 
     };
@@ -122,10 +181,29 @@
 
 
 
-var o = new Fvalidate('myForm')
+var o = new Fvalidate('myForm',[{
+    name:'user',
+    rules: ['required',/^[\u4e00-\u9fa5]+$/i],
+    messages:  ['这项必须填','必须为中文'],
+    callback:function(){},
+    ignore:false
+},{
+    name:'sure',
+    rules: ['required'],
+    messages:  ['这项必须填'],
+    callback:function(){},
+    ignore:false
+},{
+    name:'mobile',
+    rules: ['required','maxLength(11)','minLength(5)'],
+    messages:  ['这项必须填','手机号超过最大长度','长度太短'],
+    callback:function(){},
+    ignore:false
+}])
 
 
 
+/*
 o.verification ('user', {
     rules: ['required',/^[\u4e00-\u9fa5]+$/i],
     messages:  ['这项必须填','必须为中文']
@@ -133,9 +211,10 @@ o.verification ('user', {
     ele.style.borderBottomColor = 'red'
     errorEle.style.color = 'red';
 });
+*/
 
 
-o.verification ('sure', {
+/*o.verification ('sure', {
     rules: ['required'],
     messages:  ['这项必须填']
 },function(){
@@ -150,7 +229,7 @@ o.verification ('mobile', {
         ele.style.borderBottomColor = 'red';
         errorEle.style.color = 'red';
     }
-});
+});*/
 
 
 
