d54904e3614b3024c40cdddab6d44d31a4a390b3
square <fxz@huizuche.com>
2016-06-27 10:37:01 +0800

    hehe

--- app.js
+++ app.js
@@ -1,195 +1,95 @@
 
-(function(win){
+;(function(win, undefined){
 
-    var Fvalidate = function(formId, arr){
+    var validator = function(form){
+        this.form = document.forms[form]
+        this.items = []
+        this.options = []
+    }
 
-        this.myForm = document.getElementById(formId);
-        this.resultErrorMessage = [];
-        this.arr = arr;
-        this.allErrorRules = {};
-        this.allErrorMessages = {};
-        this.testHook = {
-            // 是否为必填
-            required: function(field) {
-                var value = field.value;
+    validator.prototype = {
+        add: function(opts){
+            this.items.push(opts.name)
+            this.options.push(opts)
+            bindHandlers.call(this,opts)
+            return this
+        }
+    }
 
-                //判断是不是单选框，多选框的可能
-                if(field.type === 'checkbox' || field.type === 'radio'){
-                    return field.checked === true;
-                }
-                return value !== null && value !== '';
-            },
-            maxLength: function(field, length){
-                var value = field.value;
-                return value.length <= length;
-            },
-            minLength: function(field, length){
-                var value = field.value;
-                return value.length >= length;
-            }
-
-        };
-        this.verification(this.arr);
-    };
-
-    Fvalidate.prototype = {
-        verification: function(arr){
-
-            //循环出每个input的验证详细信息
-            for(var i = 0, j = arr.length; i < j; i++){
-
-                var inputName = arr[i].name;
-                var event = arr[i].event || 'input';
-                var inputRules = arr[i].rules;
-                var inputMessages = arr[i].messages;
-                var inputIgnore = arr[i].ignore;
+    win.validator = validator
 
 
+    /****************私有方法**********************************/
 
+    //绑定验证事件
+    function bindHandlers(opts){
+        var el = this.form[opts.name], theSame
+        if(opts.sameTo){ theSame = this.form[opts.sameTo]}
+        inputFn = inputHandler.call(this,opts)
 
-
-
-                this.myForm[inputName].addEventListener(event,function(){
-
-                },false)
-
-
-                //循环出每条验证规则
-                for(var k = 0; k < inputRules.length; k++){
-
-                    var result;
-                    var inputEle = this.myForm[inputName];
-                    //当规则不是字符串(是正则)时
-                    if(typeof inputRules[k] != 'string'){
-                        result = inputRules[k].test(inputEle.value);
-                        if(!result){
-                            this.allErrorRules[inputName] = this.allErrorRules[inputName] || [];
-                            this.allErrorRules[inputName].push(inputRules[k]);
-                            this.allErrorMessages[inputName] = this.allErrorMessages[inputName] || [];
-                            this.allErrorMessages[inputName].push(inputMessages[k]);
-                        }
-                    }else{       //当规则是字符串时
-
-                        var ruleArr = /(\w+)/ig.exec(inputRules[k]);
-                        //不带参数
-                        if(ruleArr[1] === ruleArr.input){
-                            result = this.testHook[ruleArr.input](inputEle);
-                            if(!result){
-                                this.allErrorRules[inputName] = this.allErrorRules[inputName] || [];
-                                this.allErrorRules[inputName].push(inputRules[k]);
-                                this.allErrorMessages[inputName] = this.allErrorMessages[inputName] || [];
-                                this.allErrorMessages[inputName].push(inputMessages[k]);
-                            }
-                        }else{
-                            //带参数的规则处理，如：maxLength
-                            ruleArr = /(\w+)\((\d+)/ig.exec(inputRules[k]);
-                            result = this.testHook[ruleArr[1]](inputEle, ruleArr[2]);
-                            if(!result){
-                                this.allErrorRules[inputName] = this.allErrorRules[inputName] || [];
-                                this.allErrorRules[inputName].push(inputRules[k]);
-                                this.allErrorMessages[inputName] = this.allErrorMessages[inputName] || [];
-                                this.allErrorMessages[inputName].push(inputMessages[k]);
-                            }
-                        }
-                    }
-
-                }
-
-                //判断这个input是否通过验证
-                var inValida = this.allErrorRules.hasOwnProperty(inputName);
-
-
-
-                //创建错误信息元素
-                var errorEle = document.createElement('span');
-                errorEle.className = 'errorMessage ' + inputName + '_errorMessage';
-
-                if(inValida){
-                    errorEle.innerHTML = this.allErrorMessages[inputName][0];
-                }
-
-
-                //默认将错误元素插入input后面
-                if(inputEle.nextSibling){
-                    inputEle.parentNode.insertBefore(errorEle,inputEle.nextSibling)
-                }else{
-                    inputEle.parentNode.appendChild(errorEle)
-                }
-
-            }
+    }
+    function inputHandler(opts){
+        return function(){
+            validate.call(this,opts)
+        }
+    }
+    //验证
+    function validate(opts){
+        var el = this.form[opts.name], reg = '', valiFn = opts.valiFn, defaultValue = el.getAttribute('placeholder')
+        if(el.value === defaultValue){ el.value = '' }
+        if(opts.rule){
 
         }
+    }
 
-    };
 
-    return win.Fvalidate = Fvalidate;
-
-})(window, undefined);
+})(window);
 
 
 
 
+var a = new validator('myForm')
 
-var o = new Fvalidate('myForm',[{
-    name:'user',
-    rules: ['required',/^[\u4e00-\u9fa5]+$/i],
-    messages:  ['这项必须填','必须为中文'],
-    callback:function(){},
-    ignore:false
-},{
-    name:'sure',
-    rules: ['required'],
-    messages:  ['这项必须填'],
-    callback:function(){},
-    ignore:false
-},{
+a.add({
+    name:'password',
+    rule:/^[a-zA-Z0-9\_\-\~\!\%\*\@\#\$\&\.\(\)\[\]\{\}\<\>\?\\\/\'\"]{3,20}$/,
+    tips:'请输入可以联系到您的手机号码！',
+    error:'对不起，您填写的手机号码格式不正确！'
+}).add({
+    name:'confirm-password',
+    rule:/^[a-zA-Z0-9\_\-\~\!\%\*\@\#\$\&\.\(\)\[\]\{\}\<\>\?\\\/\'\"]{3,20}$/,
+    sameTo:'password',
+    error:'你填写的密码不正确或者和原密码不同'
+}).add({
     name:'mobile',
-    rules: ['required','maxLength(11)','minLength(5)'],
-    messages:  ['这项必须填','手机号超过最大长度','长度太短'],
-    callback:function(){},
-    ignore:false
-}])
+    rule:/^[1-9]\d{10}$/,
+    tips:'请输入可以联系到您的手机号码！',
+    error:'对不起，您填写的手机号码格式不正确！'
+}).add({
+    name:'email',
+    rule:/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+    tips:'请输入您常用的E-mail邮箱号，以便我们联系您，为您提供更好的服务！',
+    error:'对不起，您填写的E-mail格式不正确！正确的格式：yourname@gmail.com。'
+})
+
+
+
+
 
 
 
 /*
-o.verification ('user', {
-    rules: ['required',/^[\u4e00-\u9fa5]+$/i],
-    messages:  ['这项必须填','必须为中文']
-},function(ele, errorEle, oneValid){
-    ele.style.borderBottomColor = 'red'
-    errorEle.style.color = 'red';
-});
-*/
-
-
-/*o.verification ('sure', {
-    rules: ['required'],
-    messages:  ['这项必须填']
-},function(){
-
-});
-
-o.verification ('mobile', {
-    rules: ['required','maxLength(11)','minLength(5)'],
-    messages:  ['这项必须填','手机号超过最大长度','长度太短']
-},function(ele, errorEle, valid){
-    if(!valid){
-        ele.style.borderBottomColor = 'red';
-        errorEle.style.color = 'red';
-    }
-});*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+* opts.beforeSubmit
+* opts.target
+* opts.sameTo
+*opts.beforeFocus
+*opts.beforeBlur
+*opts.afterBlur
+*opts.afterChange
+*opts.onkeypress
+*opts.action
+*opts.rule_type
+*opts.rule
+*
+*
+* */
